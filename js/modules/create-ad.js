71e3486675472dfe0a15f63f4fcab91bf00c8947
kenmar7in <kenmar7in@yandex.com>
2022-03-17 15:43:54 +0300

    notes from mentor

--- js/modules/create-ad.js
+++ js/modules/create-ad.js
@@ -13,34 +13,42 @@ const createAd = function(data) {
     adItem.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
     adItem.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
 
-    const adFeaturesContainer = adItem.querySelector('.popup__features');
-    const adFeaturesList = adFeaturesContainer.querySelectorAll('.popup__feature');
-    const dataFeaturesList = offer.features;
+    const createFeatures = function() {
+      const adFeaturesContainer = adItem.querySelector('.popup__features');
+      const adFeaturesList = adFeaturesContainer.querySelectorAll('.popup__feature');
+      const dataFeaturesList = offer.features;
 
-    // adFeaturesList больше, чем dataFeaturesList
+      adFeaturesList.forEach((adFeaturesItem) => {
+        const isNecessary = dataFeaturesList.some((dataFeatureItem) => adFeaturesItem.classList.contains(`popup__feature--${dataFeatureItem}`));
+        if (!isNecessary) { adFeaturesItem.remove(); }
+      });
 
-    adFeaturesList.forEach((adFeaturesItem) => {
-      const isNecessary = dataFeaturesList.some((dataFeatureItem) => adFeaturesItem.classList.contains(`popup__feature--${dataFeatureItem}`));
+      return adFeaturesContainer;
+    };
 
-      if (!isNecessary) { adFeaturesItem.remove(); }
-    });
+    createFeatures();
 
     const adDescription = adItem.querySelector('.popup__description').textContent = offer.description;
-
     if (!adDescription) { adDescription.remove(); }
 
-    const adPhotosContainer = adItem.querySelector('.popup__photos');
-    const adPhotosList = adPhotosContainer.children;
-    const dataPhotosList = offer.photos;
+    const createPhotos = function() {
+      const adPhotosContainer = adItem.querySelector('.popup__photos');
+      const adPhotosList = adPhotosContainer.children;
+      const dataPhotosList = offer.photos;
 
-    dataPhotosList.forEach((dataPhotoItem, i) => {
-      if (!adPhotosList[i]) {
-        const adPhoto = adPhotosList[i - 1].cloneNode(true);
-        adPhotosContainer.appendChild(adPhoto);
-      }
+      dataPhotosList.forEach((dataPhotoItem, i) => {
+        if (!adPhotosList[i]) {
+          const adPhoto = adPhotosList[i - 1].cloneNode(true);
+          adPhotosContainer.appendChild(adPhoto);
+        }
 
-      adPhotosList[i].src = dataPhotoItem;
-    });
+        adPhotosList[i].src = dataPhotoItem;
+      });
+
+      return adPhotosContainer;
+    };
+
+    createPhotos();
 
     adFragment.appendChild(adItem);
   });
