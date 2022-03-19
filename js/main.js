dee11216328628d0c6fcc086b1cc68fe5b82b3ff
kenmar7in <kenmar7in@yandex.com>
2022-03-19 14:37:29 +0300

    added functions for set inactive/active states of page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {createData} from './modules/create-data.js';
 import {createAd} from './modules/create-ad.js';
+import {setInactiveState, setActiveState} from './modules/set-state.js';
 
 const adContainer = document.querySelector('.map__canvas');
 adContainer.appendChild(createAd(createData));
